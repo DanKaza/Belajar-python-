ecdc67a5d6f5caa747be77c7a453d7c3b29dd937
Akazano_abe <mubarrokwildan1@gmail.com>
2025-07-21 03:44:38 +0700

    cukurukuk wowop
    tambah wok

---
+++ Belajar pakai lambda yang mirip sama def.py
@@ -0,0 +1,11 @@
+#mari membuat fungsi lambda
+#gw contohin aja nih
+def kali (x,y):
+    return x * y
+print(kali(2, 3))  # Output: 6
+
+#make lambda
+kali = lambda x, y: x * y
+print(kali(3, 6)) 
+
+#sama kan?tapi lambda lebih ringkas dan singkat
